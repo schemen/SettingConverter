c3231d9263e547cad37c0b0c86143688c66265af
Schemen <me@schemen.me>
2023-09-10 21:37:08 +0200

    Updates

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pypandoc
 
 def increment_header_levels(md_content):
     # Increment the level of each header by 2
-    md_content = re.sub(r'^(#{1,6})', r'\1##', md_content, flags=re.MULTILINE)
+    md_content = re.sub(r'^(#{1,6})', r'\1#', md_content, flags=re.MULTILINE)
 
     return md_content
 
@@ -63,9 +63,32 @@ def process_folder(folder_path):
     return combined_md
 
 
+def process_folder_nobook(folder_path):
+    combined_md = ""
+    used_headers = set()
+
+    for root, dirs, files in os.walk(folder_path):
+        title1 = os.path.basename(root)  # Use the folder name as level 1 title
+        if title1 not in used_headers:
+            combined_md += f"# {title1}\n\n"
+            used_headers.add(title1)
+        for file in files:
+            if file.endswith('.md'):
+                with open(os.path.join(root, file), 'r') as md_file:
+                    md_content = md_file.read()
+                    md_content = increment_header_levels(md_content)
+                    md_content = adjust_links(md_content, root)
+                    md_content = remove_toc(md_content)
+                    md_content = ensure_newline(md_content)
+                    title2 = os.path.splitext(file)[0]  # Use the file name as level 2 title
+                    combined_md += f"# {title2}\n\n{md_content}\n\n"
+
+    return combined_md
+
 def convert_md_to_odt(combined_md, output_odt_file):
     # Convert markdown to odt
     pypandoc.convert_text(combined_md, 'odt', format='md', outputfile=output_odt_file, extra_args=['--toc', '--reference-doc=reference.odt'])
+    #pypandoc.convert_text(combined_md, 'odt', format='md', outputfile=output_odt_file, extra_args=['--toc', '--reference-doc=reference-dmnotes.odt'])
 
 
 def write_to_file(content, output_file):
@@ -78,7 +101,8 @@ def main():
     output_md_file = './combined.md'
     output_odt_file = './combined.odt'
 
-    combined_md = process_folder(folder_path)
+    #combined_md = process_folder(folder_path)
+    combined_md = process_folder_nobook(folder_path)
     write_to_file(combined_md, output_md_file)
 
     convert_md_to_odt(combined_md, output_odt_file)
